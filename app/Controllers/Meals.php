c946175681f88afdeb78d22f441e8a6657915573
Javeriya <100342410+Jknoorisys@users.noreply.github.com>
2024-02-28 16:48:45 +0530

    Add provider name to meal selection query

--- app/Controllers/Meals.php
+++ app/Controllers/Meals.php
@@ -511,7 +511,7 @@ class Meals extends ResourceController
                  $isExist = $db->table('tbl_meals as m')
                     ->join('tbl_cuision_master as c','c.id = m.cuisine_id')
                     ->join('tbl_provider as p','p.id = m.provider_id')
-                    ->select('m.*, c.name as cuisine_name')
+                    ->select('m.*, c.name as cuisine_name, CONCAT(p.firstname, " ", p.lastname) as provider_name')
                     ->where('m.id', $meals_id)
                     ->get()->getRowArray();
 
